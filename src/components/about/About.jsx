93423f76c156274d3020b82b6018abda5bc54c12
goldipl <mgodlewski92@gmail.com>
2022-03-03 19:08:43 +0100

    correct classes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,16 +3,16 @@ import "./about.css";
 const About = () => {
   return (
     <div className="about_wrapper">
-      <div className="about_wrapper-aboutMeText">
-        <h1 className="about_wrapper-aboutMeText-title">Welcome!</h1>
-        <p className="about_wrapper-aboutMeText-paragraph">
+      <div className="about_wrapper-aboutText">
+        <h1 className="about_wrapper-aboutText-title">Welcome!</h1>
+        <p className="about_wrapper-aboutText-paragraph">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias numquam repellendus at nobis commodi nisi officia, praesentium cumque rerum nulla error maxime vitae tenetur voluptas deleniti eius. Aliquid, amet adipisci!
           Repudiandae, aspernatur delectus fuga ipsam saepe quos architecto modi voluptate minus, dolorum quae blanditiis sapiente labore temporibus deserunt impedit in vel, ullam neque aliquid nisi obcaecati ducimus facilis quia. Dolorum!
           Nesciunt animi earum facere, porro sequi architecto natus similique cum non, obcaecati odit hic dolores quidem corrupti, dolore esse et qui distinctio. Quod, modi harum. Rerum saepe itaque officia doloribus?
           Facere quisquam repellat quaerat alias harum dicta delectus, nihil, illum deserunt aliquam id, eveniet tempora assumenda ut modi minus ipsa error eum. Dolor quasi doloribus perspiciatis incidunt nulla sequi libero!
           Cumque, obcaecati? Aliquid ducimus voluptatem sequi impedit voluptatum voluptate dolor quos vero fugit. Itaque corporis sed, alias voluptas voluptatum aperiam? Maiores sapiente incidunt in, fugit tenetur odit totam fuga culpa!
         </p>
-        <p className="about_wrapper-aboutMeText-paragraph">
+        <p className="about_wrapper-aboutText-paragraph">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias numquam repellendus at nobis commodi nisi officia, praesentium cumque rerum nulla error maxime vitae tenetur voluptas deleniti eius. Aliquid, amet adipisci!
           Repudiandae, aspernatur delectus fuga ipsam saepe quos architecto modi voluptate minus, dolorum quae blanditiis sapiente labore temporibus deserunt impedit in vel, ullam neque aliquid nisi obcaecati ducimus facilis quia. Dolorum!
           Nesciunt animi earum facere, porro sequi architecto natus similique cum non, obcaecati odit hic dolores quidem corrupti, dolore esse et qui distinctio. Quod, modi harum. Rerum saepe itaque officia doloribus?

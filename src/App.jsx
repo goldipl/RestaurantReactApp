06e362f06355ca28bc0b813b1f75af4ae52817d5
goldipl <mgodlewski92@gmail.com>
2022-03-03 18:19:13 +0100

    fix errors with imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import About from "./components/about/About";
-import Header from "./components/header/Header";
-import Nav from "./components/nav/Nav";
+import Nav from "./components/nav/Nav.jsx";
+import About from "./components/about/About.jsx";
+import Header from "./components/header/Header.jsx";
 
 const App = () => {
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,30 +1,32 @@
 import "./nav.css";
-import "./nav.js";
 
 const Nav = () => {
   return (
-    <nav>
-      <a href="./../../../public/index.html" class="logo">Restaurant</a>
-      <ul class="desktopMenu">
-        <li><a href="#">Main Page</a></li>
-        <li><a href="#">Menu</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-
-      <div class="hamburger">
-        <div class="bar1"></div>
-        <div class="bar2"></div>
-        <div class="bar3"></div>
-      </div>
-
-      <div class="mobileMenu">
-        <ul class="mobileMenu_li">
+    <>
+      <nav>
+        <a href="./../../../public/index.html" class="logo">Restaurant</a>
+        <ul class="desktopMenu">
           <li><a href="#">Main Page</a></li>
           <li><a href="#">Menu</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
-      </div>
-    </nav>
+
+        <div class="hamburger">
+          <div class="bar1"></div>
+          <div class="bar2"></div>
+          <div class="bar3"></div>
+        </div>
+
+        <div class="mobileMenu">
+          <ul class="mobileMenu_li">
+            <li><a href="#">Main Page</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+      <script src="./nav.js"></script>
+    </>
   );
 };
 

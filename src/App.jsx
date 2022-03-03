76687ca35d621dba6853ea590baea335811908c7
goldipl <mgodlewski92@gmail.com>
2022-03-03 13:33:05 +0100

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import About from "./components/about/About";
+import Header from "./components/header/Header";
+
+const App = () => {
+
+  return (
+    <>
+      <div className="main_wrapper">
+        <Header />
+        <About />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import "./header.css";
+import Background from "../../img/wooden_background.jpg";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="header-wrapper_background">
+        <div className="header-wrapper_background_image"></div>
+        <img src={Background} alt="Wooden Background" className="header-wrapper_background_img" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;

fdfcf66fb7bf48c396bbcc077efbb067427bc729
goldipl <mgodlewski92@gmail.com>
2022-03-04 11:02:44 +0100

    add Menu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Nav from "./components/nav/Nav.jsx";
 import Header from "./components/header/Header.jsx";
 import About from "./components/about/About.jsx";
+import Menu from "./components/menu/Menu.jsx";
 import Contact from "./components/contact/Contact.jsx";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Nav />
         <Header />
         <About />
+        <Menu />
         <Contact />
       </div>
     </>
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,20 @@
+import "./menu.scss";
+
+const Menu = () => {
+  return (
+    <div className="menu_wrapper" id="menu">
+      <div className="menu_wrapper-menuText">
+        <h1 className="menu_wrapper-menuText-title">Menu</h1>
+        <p className="menu_wrapper-menuText-paragraph">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias numquam repellendus at nobis commodi nisi officia, praesentium cumque rerum nulla error maxime vitae tenetur voluptas deleniti eius. Aliquid, amet adipisci!
+          Repudiandae, aspernatur delectus fuga ipsam saepe quos architecto modi voluptate minus, dolorum quae blanditiis sapiente labore temporibus deserunt impedit in vel, ullam neque aliquid nisi obcaecati ducimus facilis quia. Dolorum!
+          Nesciunt animi earum facere, porro sequi architecto natus similique cum non, obcaecati odit hic dolores quidem corrupti, dolore esse et qui distinctio. Quod, modi harum. Rerum saepe itaque officia doloribus?
+          Facere quisquam repellat quaerat alias harum dicta delectus, nihil, illum deserunt aliquam id, eveniet tempora assumenda ut modi minus ipsa error eum. Dolor quasi doloribus perspiciatis incidunt nulla sequi libero!
+          Cumque, obcaecati? Aliquid ducimus voluptatem sequi impedit voluptatum voluptate dolor quos vero fugit. Itaque corporis sed, alias voluptas voluptatum aperiam? Maiores sapiente incidunt in, fugit tenetur odit totam fuga culpa!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;

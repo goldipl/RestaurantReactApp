0236f52d2dcb41bc88dd1690eb3a518aa26378a5
goldipl <mgodlewski92@gmail.com>
2022-03-04 10:51:52 +0100

    styling & correct classes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="about_wrapper">
       <div className="about_wrapper-aboutText">
-        <h1 className="about_wrapper-aboutText-title">Welcome!</h1>
+        <h1 className="about_wrapper-aboutText-title_mainTitle">Traditional Polish cuisine</h1>
         <p className="about_wrapper-aboutText-paragraph">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias numquam repellendus at nobis commodi nisi officia, praesentium cumque rerum nulla error maxime vitae tenetur voluptas deleniti eius. Aliquid, amet adipisci!
           Repudiandae, aspernatur delectus fuga ipsam saepe quos architecto modi voluptate minus, dolorum quae blanditiis sapiente labore temporibus deserunt impedit in vel, ullam neque aliquid nisi obcaecati ducimus facilis quia. Dolorum!
@@ -12,6 +12,7 @@ const About = () => {
           Facere quisquam repellat quaerat alias harum dicta delectus, nihil, illum deserunt aliquam id, eveniet tempora assumenda ut modi minus ipsa error eum. Dolor quasi doloribus perspiciatis incidunt nulla sequi libero!
           Cumque, obcaecati? Aliquid ducimus voluptatem sequi impedit voluptatum voluptate dolor quos vero fugit. Itaque corporis sed, alias voluptas voluptatum aperiam? Maiores sapiente incidunt in, fugit tenetur odit totam fuga culpa!
         </p>
+        <h1 className="about_wrapper-aboutText-title">We love it!</h1>
         <p className="about_wrapper-aboutText-paragraph">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias numquam repellendus at nobis commodi nisi officia, praesentium cumque rerum nulla error maxime vitae tenetur voluptas deleniti eius. Aliquid, amet adipisci!
           Repudiandae, aspernatur delectus fuga ipsam saepe quos architecto modi voluptate minus, dolorum quae blanditiis sapiente labore temporibus deserunt impedit in vel, ullam neque aliquid nisi obcaecati ducimus facilis quia. Dolorum!

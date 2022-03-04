5c89f405e8b9d876d9dca0cf0a8572ca7d09ecb4
goldipl <mgodlewski92@gmail.com>
2022-03-04 15:15:03 +0100

    add footer component with scss

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer_wrapper">
+        Restaurant &copy; 2022
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

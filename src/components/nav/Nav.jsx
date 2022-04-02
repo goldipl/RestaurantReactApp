e0e5692ed1a65aca8fa0899747b0ae2be465d9dc
goldipl <mgodlewski92@gmail.com>
2022-04-02 12:58:20 +0200

    correct class to className jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,22 +4,22 @@ const Nav = () => {
   return (
     <>
       <nav id="nav">
-        <a href="./../../../public/index.html" class="logo">Restaurant</a>
-        <ul class="desktopMenu">
+        <a href="./../../../public/index.html" className="logo">Restaurant</a>
+        <ul className="desktopMenu">
           <li><a href="#nav">Main Page</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#menu">Menu</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
 
-        <div class="hamburger">
-          <div class="bar1"></div>
-          <div class="bar2"></div>
-          <div class="bar3"></div>
+        <div className="hamburger">
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
         </div>
 
-        <div class="mobileMenu">
-          <ul class="mobileMenu_li">
+        <div className="mobileMenu">
+          <ul className="mobileMenu_li">
             <li><a href="#nav">Main Page</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#menu">Menu</a></li>
